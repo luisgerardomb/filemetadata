51e3bbe8ad9704cb28a3f5f7f44f75bc2bb7a930
lgmarzoa <marzoalg@sanborns.com.mx>
2023-10-20 12:18:31 -0600

    added funtionality

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-var express = require('express');
-var cors = require('cors');
+let express = require('express');
+let cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+let fs = require('fs');
+const multer = require("multer");
+let app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+let storage = multer.memoryStorage();
+let upload = multer({storage: storage});
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let jsonData;
 
+  if(req.file === undefined){
+    res.status(400).json({message: 'No file to upload'});
+  } else {
+    jsonData = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    }
+
+    res.status(200).json(jsonData);
+  }
+
+  
+});
 
 
 const port = process.env.PORT || 3000;
